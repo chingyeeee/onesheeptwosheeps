294cf403a5a4b314618bb730524368102abafbc5
ronnie <ronnieyeung@fontrip.com>
2023-02-03 14:38:59 +0800

    add browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 
-const Home = lazy(() => import("./assets/components/Home"));
+const Home = lazy(() => import("./assets/pages/Home"));
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<h1 className="h-screen">loading...</h1>}>
-        <Routes>
-          <Route path="/" index element={<Home />} />
-        </Routes>
-      </Suspense>
+      <BrowserRouter>
+        <Suspense fallback={<h1 className="h-screen">loading...</h1>}>
+          <Routes>
+            <Route path="/" index element={<Home />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </div>
   );
 }
