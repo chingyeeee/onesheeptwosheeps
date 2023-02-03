b19c38055c1648a9fd7a703a76cc7b71a3ec2b60
ronnie <ronnieyeung@fontrip.com>
2023-02-03 14:38:22 +0800

    add extend colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lakegreen: "#a6e2c5",
+        lightgreen: "#20d371",
+        lightpurple: "#ff00c9",
+        red: "#ff4a03",
+        yellow: "#fffa6f",
+        purple: "#a9a9ff",
+        darkgreen: "#004943",
+      },
+    },
+    fontFamily: {},
   },
   plugins: [],
 };
