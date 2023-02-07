15de2ac334c042e39e5839fd57a5366e6d66139d
ronnie <ronnieyeung@fontrip.com>
2023-02-07 17:26:03 +0800

    add Welcome component

--- src/assets/components/Welcome.jsx
+++ src/assets/components/Welcome.jsx
@@ -1,13 +1,21 @@
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import Logo from "../images/nav/logo-default.svg";
+import useImage from "use-image";
+import { Image } from "react-konva";
+import { getImageUrl } from "../utils/getImageUrl";
 
-const Welcome = () => {
-  return (
-    <div>
-      <LazyLoadImage src={Logo} width={"100%"} alt="logo" effect="blur" />
-    </div>
-  );
+const Welcome = ({ width, height, logoType }) => {
+  const logo = getImageUrl("nav", "logo-default.svg");
+  const [image] = useImage(Logo);
+  return <Image image={image} width={width} height={height} />;
 };
+
+// const Welcome = () => {
+//   return (
+//     <div>
+//       <LazyLoadImage src={Logo} width={"100%"} alt="logo" effect="blur" />
+//     </div>
+//   );
+// };
 
 export default Welcome;
