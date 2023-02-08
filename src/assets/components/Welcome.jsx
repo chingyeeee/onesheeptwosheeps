7ae506a5f00d2f82048a5cbdfed6a0cd207b871f
ronnie <ronnieyeung@fontrip.com>
2023-02-08 15:46:39 +0800

    add handleWelcomeColor function

--- src/assets/components/Welcome.jsx
+++ src/assets/components/Welcome.jsx
@@ -1,21 +1,29 @@
 import "react-lazy-load-image-component/src/effects/blur.css";
-import Logo from "../images/nav/logo-default.svg";
 import useImage from "use-image";
 import { Image } from "react-konva";
 import { getImageUrl } from "../utils/getImageUrl";
 
-const Welcome = ({ width, height, logoType }) => {
-  const logo = getImageUrl("nav", "logo-default.svg");
-  const [image] = useImage(Logo);
+const Welcome = ({ width, height, color }) => {
+  function handleWelcomeColor(color) {
+    switch (color) {
+      case "color-default.svg":
+        return "welcome-default.svg";
+      case "color1.svg":
+        return "welcome-1.svg";
+      case "color2.svg":
+        return "welcome-2.svg";
+      case "color3.svg":
+        return "welcome-3.svg";
+      case "color4.svg":
+        return "welcome-4.svg";
+      case "color5.svg":
+        return "welcome-5.svg";
+    }
+  }
+
+  const logo = getImageUrl("nav", handleWelcomeColor(color));
+  const [image] = useImage(logo);
   return <Image image={image} width={width} height={height} />;
 };
-
-// const Welcome = () => {
-//   return (
-//     <div>
-//       <LazyLoadImage src={Logo} width={"100%"} alt="logo" effect="blur" />
-//     </div>
-//   );
-// };
 
 export default Welcome;
