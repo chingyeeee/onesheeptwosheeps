958ee574985c90ac31f64f39ae65ea2d9b60d413
ronnie <ronnieyeung@fontrip.com>
2023-02-09 14:05:38 +0800

    render cardItems

--- src/assets/components/Background.jsx
+++ src/assets/components/Background.jsx
@@ -1,95 +1,35 @@
-import { Fragment, useRef, useEffect, useState } from "react";
-import { Stage, Layer, Rect, Transformer } from "react-konva";
+import { useEffect, useState } from "react";
+import { Stage, Layer, Image } from "react-konva";
+import { getImageUrl } from "../utils/getImageUrl";
 import Welcome from "./Welcome";
-
-const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
-  const shapeRef = useRef();
-  const trRef = useRef();
-
-  useEffect(() => {
-    if (isSelected) {
-      // we need to attach transformer manually
-      trRef.current.nodes([shapeRef.current]);
-      trRef.current.getLayer().batchDraw();
-    }
-  }, [isSelected]);
-
-  return (
-    <Fragment>
-      <Rect
-        onClick={onSelect}
-        onTap={onSelect}
-        ref={shapeRef}
-        {...shapeProps}
-        draggable
-        onDragEnd={(e) => {
-          onChange({
-            ...shapeProps,
-            x: e.target.x(),
-            y: e.target.y(),
-          });
-        }}
-        onTransformEnd={(e) => {
-          // transformer is changing scale of the node
-          // and NOT its width or height
-          // but in the store we have only width and height
-          // to match the data better we will reset scale on transform end
-          const node = shapeRef.current;
-          const scaleX = node.scaleX();
-          const scaleY = node.scaleY();
-
-          // we will reset it back
-          node.scaleX(1);
-          node.scaleY(1);
-          onChange({
-            ...shapeProps,
-            x: node.x(),
-            y: node.y(),
-            // set minimal value
-            width: Math.max(5, node.width() * scaleX),
-            height: Math.max(node.height() * scaleY),
-          });
-        }}
-      />
-      {isSelected && (
-        <Transformer
-          ref={trRef}
-          boundBoxFunc={(oldBox, newBox) => {
-            // limit resize
-            if (newBox.width < 5 || newBox.height < 5) {
-              return oldBox;
-            }
-            return newBox;
-          }}
-        />
-      )}
-    </Fragment>
-  );
-};
-
-const initialRectangles = [
-  {
-    x: 10,
-    y: 10,
-    width: 100,
-    height: 100,
-    fill: "red",
-    id: "rect1",
-  },
-  {
-    x: 150,
-    y: 150,
-    width: 100,
-    height: 100,
-    fill: "green",
-    id: "rect2",
-  },
-];
+import useImage from "use-image";
+import DragAndTransformImage from "./CardImage";
+import MyImage from "./CardImage";
 
 const Background = ({ welcomeToEnabled, color }) => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  //cardItems
+  const [cardItems, setCardItems] = useState([
+    {
+      x: 10,
+      y: 10,
+      width: 300,
+      height: 300,
+      id: "rect1",
+    },
+    {
+      x: 150,
+      y: 150,
+      width: 300,
+      height: 300,
+      id: "rect2",
+    },
+  ]);
+  //selectedItem
+  const [selectedId, selectShape] = useState(null);
 
+  //配合螢幕調整canvas
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
@@ -101,28 +41,8 @@ const Background = ({ welcomeToEnabled, color }) => {
     };
   }, []);
 
-  //logo
-  // const [logoType, setLogoType] = useState("../images/nav/logo-default.svg");
-
-  //cardItems
-  const [rectangles, setRectangles] = useState(initialRectangles);
-  const [selectedId, selectShape] = useState(null);
-
-  const checkDeselect = (e) => {
-    // deselect when clicked on empty area
-    const clickedOnEmpty = e.target === e.target.getStage();
-    if (clickedOnEmpty) {
-      selectShape(null);
-    }
-  };
-
   return (
-    <Stage
-      width={windowWidth}
-      height={windowHeight}
-      onMouseDown={checkDeselect}
-      onTouchStart={checkDeselect}
-    >
+    <Stage width={windowWidth} height={windowHeight}>
       <Layer>
         {welcomeToEnabled && (
           <Welcome
@@ -131,24 +51,7 @@ const Background = ({ welcomeToEnabled, color }) => {
             color={color}
           />
         )}
-
-        {rectangles.map((rect, i) => {
-          return (
-            <Rectangle
-              key={i}
-              shapeProps={rect}
-              isSelected={rect.id === selectedId}
-              onSelect={() => {
-                selectShape(rect.id);
-              }}
-              onChange={(newAttrs) => {
-                const rects = rectangles.slice();
-                rects[i] = newAttrs;
-                setRectangles(rects);
-              }}
-            />
-          );
-        })}
+        <MyImage />
       </Layer>
     </Stage>
   );
