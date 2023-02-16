a4db6d2a8f5efff522ea2223d2c0e4a1efe14979
ronnie <ronnieyeung@fontrip.com>
2023-02-16 17:15:23 +0800

    add steps in DreamCard

--- src/assets/pages/DreamCard.jsx
+++ src/assets/pages/DreamCard.jsx
@@ -1,10 +1,20 @@
 import { useState, lazy } from "react";
 import Rules from "../components/Rules";
+import WriteDownYourName from "../components/WriteDownYourName";
 
 const DreamCard = () => {
-  const [showRules, setShowRules] = useState(true);
+  const [step, setStep] = useState(1);
 
-  return <>{showRules ? <Rules /> : ""}</>;
+  function nextStep() {
+    setStep(step + 1);
+  }
+
+  return (
+    <div className="h-screen">
+      {step === 1 && <Rules nextStep={nextStep} />}
+      {step === 2 && <WriteDownYourName nextStep={nextStep} />}
+    </div>
+  );
 };
 
 export default DreamCard;
