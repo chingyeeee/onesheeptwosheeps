d1309f23b7766a141196e1f628d7ceff6a7bee07
ronnie <ronnieyeung@fontrip.com>
2023-02-20 18:56:14 +0800

    add change stroke color function

--- src/assets/components/WriteDownYourName.jsx
+++ src/assets/components/WriteDownYourName.jsx
@@ -1,14 +1,19 @@
-import { Stage, Layer, Line, Image } from "react-konva";
+import { Stage, Layer, Line } from "react-konva";
 import { ReactComponent as Title } from "../images/writeDownYourName/writedownyourname.svg";
 import { useState, useRef, useEffect } from "react";
+import Reset from "../images/sign/reset.svg";
+import Save from "../images/sign/save.svg";
+import HoverReset from "../images/sign/hoverreset.svg";
+import HoverSave from "../images/sign/hoversave.svg";
 
-const SignatureCanvas = () => {
+const SignatureCanvas = ({ nextStep }) => {
   const [lines, setLines] = useState([]);
   const isDrawing = useRef(false);
   const stageRef = useRef();
   const backgroundRef = useRef();
   const [color, setColor] = useState("#000000");
   const [canvaWidth, setCanvaWidth] = useState(null);
+  const [signImgPath, setSignImgPath] = useState(null);
 
   const handleMouseDown = () => {
     isDrawing.current = true;
@@ -35,8 +40,8 @@ const SignatureCanvas = () => {
     isDrawing.current = false;
   };
 
-  const handleColorChange = (event) => {
-    setColor(event.target.value);
+  const handleColorChange = (color) => {
+    setColor(color);
   };
 
   const handleClear = () => {
@@ -46,6 +51,7 @@ const SignatureCanvas = () => {
   const handleSave = () => {
     const dataURL = stageRef.current.toDataURL();
     console.log(dataURL);
+    lines.length !== 0 && nextStep();
   };
 
   useEffect(() => {
@@ -57,7 +63,7 @@ const SignatureCanvas = () => {
 
   return (
     <div className="max-w-[80vw] h-[75vh] mx-auto " ref={backgroundRef}>
-      <div className="bg-signBlock bg-no-repeat bg-center bg-contain mt-6">
+      <div className="bg-signBlock bg-no-repeat bg-center bg-contain mt-6 relative">
         <Stage
           width={canvaWidth}
           height={window.innerHeight * 0.6}
@@ -76,22 +82,76 @@ const SignatureCanvas = () => {
                 strokeWidth={5}
                 tension={0.5}
                 lineCap="round"
-                globalCompositeOperation={
-                  line.tool === "eraser" ? "destination-out" : "source-over"
-                }
               />
             ))}
           </Layer>
         </Stage>
+        <p
+          className={`absolute inset-0 text-center h-min m-auto text-gray-300 z-[-1] ${
+            lines.length !== 0 && "hidden"
+          }`}
+        >
+          （ 寫 下 你 的 名 字 ）
+        </p>
       </div>
-      <div className="flex gap-4 justify-center mt-4">
-        <label>Color:</label>
-        <input type="color" value={color} onChange={handleColorChange} />
-        <div
-          className="w-[3.5rem] h-[3.5rem] cursor-pointer bg-save bg-center bg-contain"
-          onClick={handleClear}
-        ></div>
-        <div className="w-[3.5rem] cursor-pointer" onClick={handleSave}></div>
+      <div className="flex gap-16 justify-center items-center mt-4">
+        <div className="flex gap-2">
+          <div
+            className="w-[1.5rem] h-[1.5rem] bg-darkgreen cursor-pointer"
+            onClick={() => handleColorChange("#004943")}
+          />
+          <div
+            className="w-[1.5rem] h-[1.5rem] bg-lightpurple cursor-pointer"
+            onClick={() => handleColorChange("#ff00c9")}
+          />
+          <div
+            className="w-[1.5rem] h-[1.5rem] bg-blue cursor-pointer"
+            onClick={() => handleColorChange("#0073f7")}
+          />
+          <div
+            className="w-[1.5rem] h-[1.5rem] bg-lakegreen cursor-pointer"
+            onClick={() => handleColorChange("#a6e2c5")}
+          />
+        </div>
+        <div className="flex gap-4">
+          <div
+            className="w-[3.5rem] cursor-pointer group"
+            onClick={handleClear}
+          >
+            <img className={`group-hover:hidden`} src={Reset} alt="reset" />
+            <img
+              className={`hidden group-hover:block`}
+              src={HoverReset}
+              alt="reset"
+            />
+          </div>
+          <div className="w-[3.5rem] cursor-pointer group" onClick={handleSave}>
+            <img className={`group-hover:hidden`} src={Save} alt="save" />
+            <img
+              className={`hidden group-hover:block`}
+              src={HoverSave}
+              alt="save"
+            />
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <div
+            className="w-[1.5rem] h-[1.5rem] bg-yellow cursor-pointer"
+            onClick={() => handleColorChange("#fffa6f")}
+          />
+          <div
+            className="w-[1.5rem] h-[1.5rem] bg-lightgreen cursor-pointer"
+            onClick={() => handleColorChange("#20d371")}
+          />
+          <div
+            className="w-[1.5rem] h-[1.5rem] bg-red cursor-pointer"
+            onClick={() => handleColorChange("#ff4a03")}
+          />
+          <div
+            className="w-[1.5rem] h-[1.5rem] bg-purple cursor-pointer"
+            onClick={() => handleColorChange("#a9a9ff")}
+          />
+        </div>
       </div>
     </div>
   );
@@ -99,10 +159,10 @@ const SignatureCanvas = () => {
 
 const WriteDownYourName = ({ nextStep }) => {
   return (
-    <>
-      <Title onClick={nextStep} />
-      <SignatureCanvas />
-    </>
+    <div className="w-screen">
+      <Title />
+      <SignatureCanvas nextStep={nextStep} />
+    </div>
   );
 };
 
