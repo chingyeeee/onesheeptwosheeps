24cc5bd30fe15e02bee2eff717c8a0b631c02099
ronnie <ronnieyeung@fontrip.com>
2023-03-23 17:29:37 +0800

    modify loading size

--- src/assets/components/Quiz.jsx
+++ src/assets/components/Quiz.jsx
@@ -1177,14 +1177,14 @@ const Question6 = ({
 const GeneratingResult = ({ setQuestionNum }) => {
   setTimeout(() => setQuestionNum(8), 3000);
   return (
-    <div className="w-[80%] md:max-w-[50%] m-auto text-center">
+    <div className="w-[75%] md:max-w-[50%] m-auto text-center">
       <div className="flex m-auto justify-center">
-        <img className="w-[5rem]" src={Loading1} alt="Loading1" />
-        <img className="w-[5rem]" src={Loading2} alt="Loading2" />
-        <img className="w-[5rem]" src={Loading3} alt="Loading3" />
-        <img className="w-[5rem]" src={Loading4} alt="Loading4" />
+        <img className="w-[3.5rem] md:w-[5rem]" src={Loading1} alt="Loading1" />
+        <img className="w-[3.5rem] md:w-[5rem]" src={Loading2} alt="Loading2" />
+        <img className="w-[3.5rem] md:w-[5rem]" src={Loading3} alt="Loading3" />
+        <img className="w-[3.5rem] md:w-[5rem]" src={Loading4} alt="Loading4" />
       </div>
-      <p className="text-lg mt-4">
+      <p className="md:text-lg mt-4">
         WHAT YOU DREAM LEADS YOU BECOMING A BETTER MAN.
       </p>
       <span className="block animate-loaded h-[1rem] bg-blue mt-4" />
