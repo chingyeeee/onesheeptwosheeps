05e78f41b84db270c492dff6db255141bd6d4171
ronnie <ronnieyeung@fontrip.com>
2023-03-23 10:46:14 +0800

    modify dream card height

--- src/assets/pages/DreamCard.jsx
+++ src/assets/pages/DreamCard.jsx
@@ -13,7 +13,7 @@ const DreamCard = () => {
   }
 
   return (
-    <div className="h-[80vh] md:h-screen w-screen">
+    <div className="h-screen w-screen">
       <Transition
         show={step === 1}
         enter="transition-opacity duration-300"
@@ -22,7 +22,7 @@ const DreamCard = () => {
         leave="transition-opacity duration-300"
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
-        className={"h-[80vh] md:h-screen"}
+        className={"h-screen"}
       >
         <Rules nextStep={nextStep} step={step} />
       </Transition>
@@ -34,7 +34,7 @@ const DreamCard = () => {
         leave="transition-opacity duration-300"
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
-        className={"h-[80vh] md:h-screen"}
+        className={"h-screen"}
       >
         <WriteDownYourName
           nextStep={nextStep}
@@ -50,7 +50,7 @@ const DreamCard = () => {
         leave="transition-opacity duration-300"
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
-        className={"h-[85vh] md:h-screen"}
+        className={"h-screen"}
       >
         <Quiz signImgPath={signImgPath} />
       </Transition>
