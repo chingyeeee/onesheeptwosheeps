bd0c812fa526dc31b261fc9a79789df05c32f913
Huang Kuan Wei <xellosiris@gmail.com>
2023-04-17 08:36:10 +0800

    disable server config in vite

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync("./cert/localhost-key.pem"),
-      cert: fs.readFileSync("./cert/localhost.pem"),
-    },
-  },
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync("./cert/localhost-key.pem"),
+  //     cert: fs.readFileSync("./cert/localhost.pem"),
+  //   },
+  // },
   plugins: [react(), svgr()],
 });
