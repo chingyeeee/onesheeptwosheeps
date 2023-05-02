98ab0ab5fc00e09b8f226432901fb57108eef42d
Huang Kuan Wei <xellosiris@gmail.com>
2023-05-02 16:49:29 +0800

    水平跑馬燈修復

--- src/components/ResultCard/Marquee.jsx
+++ src/components/ResultCard/Marquee.jsx
@@ -9,7 +9,9 @@ export default function Marquee({ image, className, vertical = true }) {
       )}>
       <div
         className={clsx(
-          vertical ? "animate-marqueeV h-[200%] flex-col w-full" : "animate-marquee w-[200%]",
+          vertical
+            ? "animate-marqueeV h-[200%] flex-col w-full"
+            : "animate-marquee w-[200%] h-full",
           "absolute m-auto flex"
         )}>
         <img className={clsx(vertical ? "h-1/2 md:h-full w-auto" : "w-1/2 h-auto")} src={image} />
@@ -19,23 +21,4 @@ export default function Marquee({ image, className, vertical = true }) {
       </div>
     </div>
   );
-}
-
-{
-  /* <div className='bg-yellow absolute left-0 w-[2rem] md:w-[5rem] min-h-inherit h-full overflow-hidden'>
-        <div className='absolute inset-x-0 m-auto flex flex-col animate-marqueeV h-[200%]'>
-          <img className='h-[50%] md:h-[100%] w-auto' src={LeftMarquee} />
-          <img className='h-[50%] md:h-[100%] w-auto' src={LeftMarquee} />
-          <img className='h-[50%] md:h-[100%] w-auto' src={LeftMarquee} />
-          <img className='h-[50%] md:h-[100%] w-auto' src={LeftMarquee} />
-        </div>
-      </div>
-      <div className='bg-purple absolute right-0 w-[2rem] md:w-[5rem] min-h-inherit h-full overflow-hidden'>
-        <div className='absolute inset-x-0 m-auto flex flex-col animate-marqueeV h-[200%]'>
-          <img className='h-[50%] md:h-[100%] w-auto' src={RightMarquee} />
-          <img className='h-[50%] md:h-[100%] w-auto' src={RightMarquee} />
-          <img className='h-[50%] md:h-[100%] w-auto' src={RightMarquee} />
-          <img className='h-[50%] md:h-[100%] w-auto' src={RightMarquee} />
-        </div>
-      </div> */
 }
