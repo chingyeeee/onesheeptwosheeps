78639b55b4fcbf65fdb747d78385f3cec3321b92
Huang Kuan Wei <xellosiris@gmail.com>
2023-05-03 17:42:42 +0800

    rule微調

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -4,14 +4,14 @@ import { ReactComponent as Finger } from "../assets/images/icons/icon-finger.svg
 const Rules = ({ nextStep }) => {
   return (
     <div className='h-full flex justify-center items-center animate-zoomIn min-h-inherit'>
-      <div className='relative flex flex-col space-y-10 w-[90%] md:w-[70%] bg-yellow rounded-[150px] md:rounded-full py-24 px-12 text-center min-h-[50%]'>
-        <RulesLogo className='md:w-[50%] w-[90%] m-auto translate-x-3 md:translate-x-0' />
-        <p className='py-5'>
+      <div className='relative flex flex-col space-y-10 w-[90%] md:w-[70%] tv:w-[90%] bg-yellow rounded-[150px] md:rounded-full tv:rounded-[150px] py-24 px-12 text-center min-h-[50%]'>
+        <RulesLogo className='md:w-[50%] w-[90%] tv:w-[90%] m-auto translate-x-3 md:translate-x-0 translate-y-3' />
+        <p className='py-5 tv:text-2xl'>
           請試著回想一個讓你印象深刻的夢，可以是任何的情境
           <br />
           不管傷心難過、開心快樂甚至是難以啟齒的夢，都請回答接下來的問題！
           <br />
-          <p className='py-3 text-sm'>
+          <p className='py-3 tv:text-xl'>
             Please try to recall a dream that impressed you, it can be any situation.
             <br />
             Regardless of whether you are sad, happy or even unspeakable dreams, <br />
@@ -21,7 +21,7 @@ const Rules = ({ nextStep }) => {
         <div
           className={`bg-blue mt-8 w-min m-auto px-6 py-3 rounded-full cursor-custom animate-remind-lightening transition hover:animate-lightening opacity-1`}
           onClick={nextStep}>
-          <Finger className='w-[42px] h-[20px] md:w-[56px] md:h-[25px] fill-yellow' />
+          <Finger className='w-[42px] h-[20px] md:w-[56px] md:h-[25px] tv:w-[150px] tv:h-[45px] fill-yellow' />
         </div>
       </div>
     </div>
